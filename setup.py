86dcf09e28815a177332148721ad5790e0677a26
Snehan Kekre <snehan.minerva@gmail.com>
2022-02-09 18:34:24 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-discourse",
+    version="0.0.1",
+    author="Snehan Kekre",
+    author_email="snehan@streamlit.io",
+    description="Streamlit component to display Streamlit-Discourse topics related to any exception.",
+    long_description="Streamlit component to display Streamlit-Discourse topics related to any exception.",
+    long_description_content_type="text/plain",
+    url="https://github.com/snehankekre/streamlit-discourse",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=["streamlit >= 1.0.0", "requests >= 2.20.0"]
+)
